dd3d521ca281b17eacf8b37481a29e7663ec7763
amr <alexmakeresponsive@gmail.com>
2020-03-02 20:05:52 +0600

    client: ticket form: calculate position for message and side buttons

--- client/src/app/template/page/service/msa/ticket/component.ts
+++ client/src/app/template/page/service/msa/ticket/component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef, Renderer2, HostListener } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import * as formFields from './form/fields';
-import {EntryDirective} from "../../../../../entry/directive";
+import MsaResponse from "../../../../../model/msa/Response";
+import {AuthService} from "../../../../../auth.service";
 
 @Component({
   selector: 'page-service-msa-ticket',
@@ -35,9 +37,13 @@ export class PageServiceMsaTicketComponent implements OnInit {
   form:FormGroup;
 
   @ViewChild('containerBtn', {static: false}) containerBtn: ElementRef;
+  @ViewChild('formMessage', {static: false}) formMessage: ElementRef;
+  @ViewChild('formWrapper', {static: false}) formWrapper: ElementRef;
 
   constructor(
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private authService: AuthService,
+    private http: HttpClient,
   ) { }
 
   ngOnInit() {
@@ -61,21 +67,41 @@ export class PageServiceMsaTicketComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
   scrollHandler(event) {
-    if (document.body.scrollTop <= 395 || document.documentElement.scrollTop <= 395) {
-      this.renderer.setStyle(this.containerBtn.nativeElement, 'top', (425 + 'px'));
+    if (document.body.scrollTop <= 396 || document.documentElement.scrollTop <= 396) {
+      const top = 246 + 150 + 30 - window.pageYOffset;
+      this.renderer.setStyle(this.containerBtn.nativeElement, 'top', (top + 'px'));
       this.renderer.setStyle(this.containerBtn.nativeElement, 'bottom', 'auto');
     }
 
-    if (document.body.scrollTop > 395 || document.documentElement.scrollTop > 395) {
-      this.renderer.setStyle(this.containerBtn.nativeElement, 'top', (27 + 'px'));
+    if (document.body.scrollTop > 396 || document.documentElement.scrollTop > 396) {
+      this.renderer.setStyle(this.containerBtn.nativeElement, 'top', (30 + 'px'));
       this.renderer.setStyle(this.containerBtn.nativeElement, 'bottom', 'auto');
     }
 
-    if ( document.body.scrollTop > (document.body.scrollHeight - 850)
-      || document.documentElement.scrollTop > (document.body.scrollHeight - 850)) {
+      const bottomBreakpoint = document.body.clientHeight - window.innerHeight - 30;
+      const bottom =  window.innerHeight + window.pageYOffset - document.body.clientHeight - 45 + 90;
+
+
+      const formMessagePositionTop = window.pageYOffset - 150 + 30;
+      const formMessagePositionBottom = document.body.clientHeight - 150 - 45 -47;
+
+    if (document.body.scrollTop <= 150 || document.documentElement.scrollTop <= 150) {
+      this.renderer.setStyle(this.formMessage.nativeElement, 'top', (0 + 'px'));
+    }
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      this.renderer.setStyle(this.formMessage.nativeElement, 'top', (formMessagePositionTop + 'px'));
+    }
+
+    if ( document.body.scrollTop > (bottomBreakpoint)
+      || document.documentElement.scrollTop > (bottomBreakpoint)) {
+
       this.renderer.setStyle(this.containerBtn.nativeElement, 'top', 'auto');
-      this.renderer.setStyle(this.containerBtn.nativeElement, 'bottom', '45px');
+      this.renderer.setStyle(this.containerBtn.nativeElement, 'bottom', (bottom + 'px'));
+
+      this.renderer.setStyle(this.formMessage.nativeElement, 'top', (formMessagePositionBottom + 'px'));
     }
+
+
   }
 
   scrollToTop() {
@@ -87,12 +113,52 @@ export class PageServiceMsaTicketComponent implements OnInit {
   }
 
   //with with forms
-  doSubmit() {
-
-    if(this.form.valid) {
-      const formData = this.form.value;
-      console.log(formData);
+  async doSubmit() {
+    if(!this.form.valid) {
+      return;
     }
+
+    this.formMessage.nativeElement.innerHTML ='Форма отправляется';
+    this.renderer.setStyle(this.formMessage.nativeElement, 'display', 'block');
+    this.renderer.setStyle(this.formWrapper.nativeElement, 'opacity', '0.5');
+
+
+    let top = window.pageYOffset;
+    if (window.pageYOffset <= 150 ) {
+      top = 0;
+    } else {
+      top = window.pageYOffset - 120;
+    }
+    this.renderer.setStyle(this.formMessage.nativeElement, 'top', (top + 'px'));
+
+    const formData = this.form.value;
+
+    return;
+
+    // do http
+    const keychain = this.authService.getKeyChain();
+
+    const token    = keychain.tokenAccessList !== null ? keychain.tokenAccessList.msa : '';
+
+    let headers = new HttpHeaders();
+        headers = headers.set('Authorization', 'Bearer ' + token);
+
+    await this.http.post<MsaResponse>(
+      'http://0.0.0.0:8204/ticket',
+      {},
+      {
+        headers: headers
+      }
+    ).toPromise()
+      .then(
+        res => {
+          console.log('form saved');
+          this.doClear();
+        },
+        rej => {
+          console.log("rej: ", rej);
+        }
+      );
   }
 
   doClear() {
