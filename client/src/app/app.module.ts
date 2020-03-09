adb6979924ece06044ca1a49bcdc07ae209ced99
amr <alexmakeresponsive@gmail.com>
2020-03-09 23:45:02 +0600

    client: refactoring: remove HttpClientModule from app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule }                        from '@angular/platform-browser';
 
-import { HttpClientModule }                     from '@angular/common/http';
 import { NgModule }                             from '@angular/core';
 
 import { AppRoutingModule }                     from './app-routing.module';
@@ -22,8 +21,6 @@ import { AppComponent }                         from './app.component';
   ],
   imports: [
     BrowserModule,
-
-    HttpClientModule,
 
     AppRoutingModule,
 
--- client/src/app/service/http/http-service.module.ts
+++ client/src/app/service/http/http-service.module.ts
@@ -1,5 +1,3 @@
-import { BrowserModule }                        from '@angular/platform-browser';
-
 import { HttpClientModule }                     from '@angular/common/http';
 import { NgModule }                             from '@angular/core';
 
